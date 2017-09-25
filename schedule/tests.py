28b097899e3c765f4f5f5ac197d54cad1b897888
Matthew Bernheimer <Krutonslol@gmail.com>
2017-09-24 20:40:12 -0400

    fixed text and added selenium tests to schedule view

--- schedule/tests.py
+++ schedule/tests.py
@@ -1,6 +1,13 @@
 
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+from users.models import FollowedShows
 
 # Create your tests here.
 
@@ -34,3 +41,46 @@ class ScheduleViewWithDateTests(TestCase):
         url = reverse('schedule:schedules_by_date', args=('2010-80-40',))
         response = self.client.get(url)
         self.assertEqual(404, response.status_code)
+
+
+class ScheduleViewSeleniumTests(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user('tom', 'test@test.com', 'password')
+        self.client.login(username=self.user.username, password='password')
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.refresh()
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def tearDown(self):
+        self.selenium.refresh()
+
+    def test_follow_show(self):
+        """When following a show the shows button should get 'btn-success' class and text should change to Following
+        """
+        self.assertFalse(FollowedShows.objects.filter(user=self.user))
+        timeout = 10
+        self.selenium.get('{}/schedule/'.format(self.live_server_url))
+        self.selenium.add_cookie({'name': 'sessionid', 'value': self.client.cookies['sessionid'].value, 'secure': False, 'path': '/'})
+        self.selenium.refresh()
+        self.selenium.get("{}/schedule/".format(self.live_server_url))
+        elements = self.selenium.find_elements_by_class_name('follow-btn')
+        first_ele = elements[0]
+        self.assertTrue('btn-primary' in first_ele.get_attribute('class'))
+        self.assertEqual(first_ele.text, 'Follow')
+        first_ele.click()
+        WebDriverWait(self.selenium, timeout).until(
+            lambda driver: driver.find_elements_by_class_name('follow-btn')[0].text == 'Following'
+        )
+        self.assertTrue('btn-success' in first_ele.get_attribute('class'))
+        self.assertEqual(first_ele.text, 'Following')
+        self.assertTrue(FollowedShows.objects.filter(user=self.user))
